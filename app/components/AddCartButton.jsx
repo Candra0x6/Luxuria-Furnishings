8b5eeefbae9090d698b871614bd89989a478197d
Candra0x6 <kurniawancandram@gmail.com>
2024-04-11 12:07:13 +0700

    style : cart button splice component

--- app/components/AddCartButton.jsx
+++ app/components/AddCartButton.jsx
@@ -1,35 +1,39 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { styled } from "nativewind";
 import { FontAwesome6 } from "@expo/vector-icons";
-const AddCartButton = ({ price }) => {
-  const ButtonWidget = styled(TouchableOpacity);
-  const ViewWidget = styled(View);
-  const TextWidget = styled(Text);
+import AddProductToCart from "../utils/addProductToCart";
+const AddCartButton = ({ price, productID }) => {
+  const { postProductToCart, isLoading } = AddProductToCart();
 
   return (
-    <ViewWidget className=" p-5 flex-row justify-between w-full flex left-0 right-0 bottom-0 border-2 border-[#dddddd] bg-white rounded-t-xl">
-      <ViewWidget className="flex flex-col">
-        <TextWidget
+    <View className=" p-5 flex-row justify-between w-full flex left-0 right-0 bottom-0 border-2 border-[#dddddd] bg-white rounded-t-xl">
+      <View className="flex flex-col">
+        <Text
           style={{ fontFamily: "InterMedium" }}
           className="text-lg text-[#707F81]"
         >
           Total Price
-        </TextWidget>
-        <TextWidget style={{ fontFamily: "InterSemiBold" }} className="text-lg">
+        </Text>
+        <Text style={{ fontFamily: "InterSemiBold" }} className="text-lg">
           {"$" + price}
-        </TextWidget>
-      </ViewWidget>
-      <ButtonWidget className="flex flex-row bg-[#3C5A5D] px-14 py-4 items-center justify-center rounded-full">
+        </Text>
+      </View>
+      <TouchableOpacity
+        disabled={isLoading}
+        onPress={() => postProductToCart(productID)}
+        className={`flex flex-row px-14 py-4 items-center justify-center rounded-full ${
+          isLoading ? "bg-[#3c5a5d8b]" : "bg-[#3C5A5D]"
+        }`}
+      >
         <FontAwesome6 name="bag-shopping" size={24} color="white" />
-        <TextWidget
+        <Text
           style={{ fontFamily: "InterMedium" }}
           className="text-lg text-white ml-2"
         >
-          Add to Cart
-        </TextWidget>
-      </ButtonWidget>
-    </ViewWidget>
+          {isLoading ? "Loading..." : " Add to Cart"}
+        </Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
